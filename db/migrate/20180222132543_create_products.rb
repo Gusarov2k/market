4caf71f00dccb613c59bf99a129b16ed953d2425
Ivan2k <motoface@yandex.ru>
2018-02-22 16:30:51 +0300

    fix bug on migration products

--- db/migrate/20180222132543_create_products.rb
+++ db/migrate/20180222132543_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-    create_table :products do |t|
-      t.string : title
-      t.text :descriotion
-      t.string :image_url
-      t.decimal :price
+    create_table 	:products do |t|
+      t.string 		:title
+      t.text 		:descriotion
+      t.string 		:image_url
+      t.decimal 	:price, precision: 8, scale: 2
 
       t.timestamps null: false
     end
